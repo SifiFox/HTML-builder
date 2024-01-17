48b0ec66204f8bd765b8c3a53bae40f011ef0b3b
SifiFox <dima.antonovich.1999@gmail.com>
2024-01-17 17:14:44 +0300

    fix: lint warnings

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+const { stdin, stdout } = process;
+const filePath = path.join(__dirname, 'write.txt');
+const output = fs.createWriteStream(filePath);
+
+stdout.write('Enter your message: ');
+stdin.on('data', (e) => {
+  if (e.includes('exit')) bye();
+  output.write(e);
+});
+
+function bye() {
+  console.log('good bye');
+  process.exit();
+}
